8855690ad0d362dae6316c09a3e97759332c77f9
任文浩 <8506326+ren-wenhao@user.noreply.gitee.com>
2020-12-29 17:06:18 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
+import ElementUI, {Message} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//导入全局样式表
+import './assets/css/global.css'
+import axios from "axios";
+//配置请求的根路径
+axios.defaults.baseURL = 'http://119.23.53.78:8888/api/private/v1/'
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
+Vue.use(ElementUI);
+
+
 
 Vue.config.productionTip = false
 
